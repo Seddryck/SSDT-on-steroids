e694c32afc111605cdc6a2bd0834944bcfb00a2f
Cédric L. Charlier <cedric.charlier@yucom.be>
2016-02-20 15:10:41 +0100

    Initial commit for demo about extensions

---
+++ 3. Schema extension/Extended/Dwh/FactMonthlyOrder.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[FactMonthlyOrder]
+(
+	[CustomerId] INT NOT NULL 
+	, [OrderMonth] INT NOT NULL 
+	, [TotalAmount] NUMERIC(10,2) NOT NULL
+    , CONSTRAINT [BK_FactMonthlyOrder] UNIQUE ([CustomerId], [OrderMonth])
+    , CONSTRAINT [FK_Customer_FactMonthlyOrder] FOREIGN KEY ([CustomerId]) REFERENCES [DimCustomer]([CustomerId])
+)
---
+++ 3. Schema extension/Extended/Dwh/MonthlyOrderByCustomer.sql
@@ -0,0 +1,28 @@
+﻿CREATE FUNCTION [dbo].[MonthlyOrderByCustomer]
+(
+	@CustomerCode CHAR(8)
+	, @Year int
+)
+RETURNS @returntable TABLE
+(
+	DisplayName VARCHAR(120)
+	, MonthDisplayName VARCHAR(50) 
+	, TotalAmount numeric(10,2)
+)
+AS
+BEGIN
+	insert @returntable
+	select
+		[CustomerName]
+		, DATENAME(month,DATEFROMPARTS(@Year, OrderMonth % 100, 1))
+		, TotalAmount
+	from
+		DimCustomer cust
+	left outer join
+		FactMonthlyOrder mo
+		on mo.CustomerId=cust.CustomerId
+	where
+		(mo.OrderMonth - (mo.OrderMonth %  100))/100 = @Year
+
+	return;
+END
