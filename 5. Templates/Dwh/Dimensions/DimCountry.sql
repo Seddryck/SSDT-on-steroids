5993a71eae46bcf24395460b8b80cb5b685ef79e
Cédric L. Charlier <cedric.charlier@yucom.be>
2016-02-20 18:11:21 +0100

    Initial commit for demo about T4 Templates

---
+++ 5. Templates/Dwh/Dimensions/DimCountry.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[DimCountry]
+(
+	[CountryId] INT IDENTITY NOT NULL PRIMARY KEY
+	, [CountryIso] CHAR(2)
+	, [DisplayName] VARCHAR(120)
+	, [Capital] VARCHAR (120), 
+    [OfficialLanguage] VARCHAR(255) NULL
+)
+
+GO
+
+CREATE UNIQUE INDEX [IX_DimCountry_Column] ON [dbo].[DimCountry] ([CountryIso])
---
+++ 5. Templates/Stg/Dimensions/DimCountry.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [stg].[DimCountry]
+(
+	[CountryIso] CHAR(2)
+	, [DisplayName] VARCHAR(120)
+	, [Capital] VARCHAR (120) 
+    , [OfficialLanguage] VARCHAR(255) NULL, 
+    CONSTRAINT [UK_DimCountry_CountryIso] UNIQUE ([CountryIso])
+)
